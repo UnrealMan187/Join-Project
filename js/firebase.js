4004689f87c12f7b243da7e078971b34181d8496
EnverShala <envershala1989@gmail.com>
2024-08-03 22:55:39 +0200

    added alphabetic Sort to loadUsers Function

--- js/firebase.js
+++ js/firebase.js
@@ -16,52 +16,54 @@ async function loadUsers(path = "/users") {
         phone: responseToJson[key]["phone"],
       });
     });
+    users.sort((a, b) => {
+      return a.name.localeCompare(b.name); // sortiere users nach dem Wert "name"
+    });
     console.log(users);
   }
-}
 
-async function addUser() {
-  let nameValue = document.getElementById("name").value;
-  let phoneValue = document.getElementById("phone").value;
-  let emailValue = document.getElementById("email").value;
-  let newUser = { name: nameValue, email: emailValue, phone: phoneValue };
-  document.getElementById("name").value = "";
-  document.getElementById("phone").value = "";
-  document.getElementById("email").value = "";
-  await postData("/users", newUser);
-  await loadUsers("/users");
-}
+  async function addUser() {
+    let nameValue = document.getElementById("name").value;
+    let phoneValue = document.getElementById("phone").value;
+    let emailValue = document.getElementById("email").value;
+    let newUser = { name: nameValue, email: emailValue, phone: phoneValue };
+    document.getElementById("name").value = "";
+    document.getElementById("phone").value = "";
+    document.getElementById("email").value = "";
+    await postData("/users", newUser);
+    await loadUsers("/users");
+  }
 
-async function postData(path = "", data = {}) {
-  await fetch(FIREBASE_URL + path + ".json", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
-}
+  async function postData(path = "", data = {}) {
+    await fetch(FIREBASE_URL + path + ".json", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+  }
 
-// id = path in firebase
+  // id = path in firebase
 
-async function deleteUser(id) {
-  await fetch(FIREBASE_URL + `/users/${id}` + ".json", {
-    method: "DELETE",
-  });
-}
+  async function deleteUser(id) {
+    await fetch(FIREBASE_URL + `/users/${id}` + ".json", {
+      method: "DELETE",
+    });
+  }
 
-async function editUser(id, data = {}) {
-  await fetch(FIREBASE_URL + `/users/${id}` + ".json", {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
-}
+  async function editUser(id, data = {}) {
+    await fetch(FIREBASE_URL + `/users/${id}` + ".json", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+  }
 
-// We use the email to identify an User because there may be 2 Users with the same Name but not same email.
-function getUserId(email) {
+  // We use the email to identify an User because there may be 2 Users with the same Name but not same email.
+  function getUserId(email) {
     if (users.length > 0) {
       for (let i = 0; i < users.length; i++) {
         if (users[i].email == email) {
@@ -71,4 +73,5 @@ function getUserId(email) {
     } else {
       return -1; // Default Value -1 means User not found
     }
+  }
 }
