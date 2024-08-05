aa81ffadf93b8374b936883e85605e1726de0a91
UnrealMan187 <p.ohrner89@gmail.com>
2024-08-05 09:09:29 +0200

    Function addContacts/editContacts

---
+++ js/contacts.js
@@ -0,0 +1,53 @@
+/**
+ * Öffnet das Pop-up-Fenster und lädt den Inhalt von addContacts.html.
+ */
+function addNewUser() {
+    fetch('addContacts.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('popup-body').innerHTML = data;
+            document.getElementById('popup').style.display = 'block';
+        });
+}
+
+
+/**
+ * Schließt das Pop-up-Fenster.
+ */
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+}
+
+
+/**
+ * Öffnet das Pop-up-Fenster und lädt den Inhalt von addContacts.html.
+ */
+function addNewUser() {
+    fetch('addContacts.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('popup-body').innerHTML = data;
+            document.getElementById('popup').style.display = 'block';
+        });
+}
+
+
+/**
+ * Öffnet das Pop-up-Fenster und lädt den Inhalt von editContacts.html.
+ */
+function editUserPopup() {
+    fetch('editContacts.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('popup-body').innerHTML = data;
+            document.getElementById('popup').style.display = 'block';
+        });
+}
+
+
+/**
+ * Schließt das Pop-up-Fenster.
+ */
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+}
